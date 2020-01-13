b2ca11e4c6719cfcd894f6e5af6abb8f633a0032
Heyllon Bastos <heyllonv@outlook.com>
2020-01-13 11:50:01 -0300

    Cavalo

--- src/xadrez/PartidaDeXadrez.java
+++ src/xadrez/PartidaDeXadrez.java
@@ -8,6 +8,7 @@ import tabuleiro.Peca;
 import tabuleiro.Posicao;
 import tabuleiro.Tabuleiro;
 import xadrez.pecas.Bispo;
+import xadrez.pecas.Cavalo;
 import xadrez.pecas.Peao;
 import xadrez.pecas.Rei;
 import xadrez.pecas.Torre;
@@ -194,9 +195,11 @@ public class PartidaDeXadrez {
 	private void configuracaoInicial() {
 		
 		coloqueNovaPeca('a', 1, new Torre(tabuleiro, Color.WHITE));
+		coloqueNovaPeca('b', 1, new Cavalo(tabuleiro, Color.WHITE));
 		coloqueNovaPeca('c', 1, new Bispo(tabuleiro, Color.WHITE));
 		coloqueNovaPeca('e', 1, new Rei(tabuleiro, Color.WHITE));
 		coloqueNovaPeca('f', 1, new Bispo(tabuleiro, Color.WHITE));
+		coloqueNovaPeca('g', 1, new Cavalo(tabuleiro, Color.WHITE));
 		coloqueNovaPeca('h', 1, new Torre(tabuleiro, Color.WHITE));
 		coloqueNovaPeca('a', 2, new Peao(tabuleiro, Color.WHITE));
 		coloqueNovaPeca('b', 2, new Peao(tabuleiro, Color.WHITE));
@@ -208,9 +211,11 @@ public class PartidaDeXadrez {
 		coloqueNovaPeca('h', 2, new Peao(tabuleiro, Color.WHITE));
 
 		coloqueNovaPeca('a', 8, new Torre(tabuleiro, Color.BLACK));
+		coloqueNovaPeca('b', 8, new Cavalo(tabuleiro, Color.BLACK));
 		coloqueNovaPeca('c', 8, new Bispo(tabuleiro, Color.BLACK));
 		coloqueNovaPeca('e', 8, new Rei(tabuleiro, Color.BLACK));
 		coloqueNovaPeca('f', 8, new Bispo(tabuleiro, Color.BLACK));
+		coloqueNovaPeca('g', 8, new Cavalo(tabuleiro, Color.BLACK));
 		coloqueNovaPeca('h', 8, new Torre(tabuleiro, Color.BLACK));
 		coloqueNovaPeca('a', 7, new Peao(tabuleiro, Color.BLACK));
 		coloqueNovaPeca('b', 7, new Peao(tabuleiro, Color.BLACK));
--- src/xadrez/pecas/Cavalo.java
+++ src/xadrez/pecas/Cavalo.java
@@ -5,15 +5,15 @@ import tabuleiro.Tabuleiro;
 import xadrez.Color;
 import xadrez.PecaDeXadrez;
 
-public class Rei extends PecaDeXadrez{
+public class Cavalo extends PecaDeXadrez{
 
-	public Rei(Tabuleiro tabuleiro, Color color) {
+	public Cavalo(Tabuleiro tabuleiro, Color color) {
 		super(tabuleiro, color);
 	}
 	
 	@Override
 	public String toString() {
-		return "K";
+		return "C";
 	}
 
 	private boolean podeMover(Posicao posicao) {
@@ -27,50 +27,42 @@ public class Rei extends PecaDeXadrez{
 		boolean[][] mat = new boolean[getTabuleiro().getLinhas()][getTabuleiro().getColunas()];
 		Posicao p = new Posicao(0, 0);
 		
-		//PARA CIMA
-		p.setValores(posicao.getLinha() - 1, posicao.getColuna());
+		p.setValores(posicao.getLinha() - 1, posicao.getColuna() - 2);
 		if(getTabuleiro().posicaoExistente(p) && podeMover(p)) {
 			mat[p.getLinha()][p.getColuna()] = true;
 		}
 		
-		//PARA BAIXO
-		p.setValores(posicao.getLinha() + 1, posicao.getColuna());
+		p.setValores(posicao.getLinha() - 2, posicao.getColuna() - 1);
 		if(getTabuleiro().posicaoExistente(p) && podeMover(p)) {
 			mat[p.getLinha()][p.getColuna()] = true;
 		}
 		
-		//PARA A ESQUERDA
-		p.setValores(posicao.getLinha(), posicao.getColuna() - 1);
+		p.setValores(posicao.getLinha() - 2, posicao.getColuna() + 1);
 		if(getTabuleiro().posicaoExistente(p) && podeMover(p)) {
 			mat[p.getLinha()][p.getColuna()] = true;
 		}
 		
-		//PARA A DIREITA
-		p.setValores(posicao.getLinha(), posicao.getColuna() + 1);
+		p.setValores(posicao.getLinha() - 1, posicao.getColuna() + 2);
+		if(getTabuleiro().posicaoExistente(p) && podeMover(p)) {
+			mat[p.getLinha()][p.getColuna()] = true;
+		}
+		 
+		p.setValores(posicao.getLinha() + 1, posicao.getColuna() + 2);
 		if(getTabuleiro().posicaoExistente(p) && podeMover(p)) {
 			mat[p.getLinha()][p.getColuna()] = true;
 		}
 		
-		//PARA NW NOROESTE 
-		p.setValores(posicao.getLinha() - 1, posicao.getColuna() - 1);
+		p.setValores(posicao.getLinha() + 2, posicao.getColuna() + 1);
 		if(getTabuleiro().posicaoExistente(p) && podeMover(p)) {
 			mat[p.getLinha()][p.getColuna()] = true;
 		}
 		
-		//PARA A NORDESTE
-		p.setValores(posicao.getLinha() - 1, posicao.getColuna() + 1);
+		p.setValores(posicao.getLinha() + 2, posicao.getColuna() - 1);
 		if(getTabuleiro().posicaoExistente(p) && podeMover(p)) {
 			mat[p.getLinha()][p.getColuna()] = true;
 		}
 		
-		//PARA A SUDOESTE
-		p.setValores(posicao.getLinha() + 1, posicao.getColuna() - 1);
-		if(getTabuleiro().posicaoExistente(p) && podeMover(p)) {
-			mat[p.getLinha()][p.getColuna()] = true;
-		}
-		
-		//PARA A SUDESTE
-		p.setValores(posicao.getLinha() + 1, posicao.getColuna() + 1);
+		p.setValores(posicao.getLinha() + 1, posicao.getColuna() - 2);
 		if(getTabuleiro().posicaoExistente(p) && podeMover(p)) {
 			mat[p.getLinha()][p.getColuna()] = true;
 		}
